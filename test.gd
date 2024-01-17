a8b1c23ce33060b5587ade1f3f4e47c5d3970c31
Brian McGovern <bmcgovern2164@gmail.com>
2024-01-17 18:15:50 -0500

    Added ability to only jump when on floor/ceiling

--- test.gd
+++ test.gd
@@ -5,18 +5,28 @@ var speed = 100
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta):
-	
-	# Add the gravity.
-	velocity.y += gravity * delta
-	# Add constant speed
-	velocity.x = speed
-		
+# Our function for jumping so we don't have to repeat.
+func jump():
 	if Input.is_action_just_pressed("ui_accept"):
 		#change gravity
 		gravity = -gravity
 		
 		#change character rotation
 		$".".scale.y = -$".".scale.y
+
+func _physics_process(delta):
+	
+	# Add the gravity.
+	velocity.y += gravity * delta
+	# Add constant speed
+	velocity.x = speed
+	
+	# Allows player to only jump when touching floor.
+	if is_on_floor():
+		jump()
+		
+	# Allows player to only jump when touching ceiling.		
+	if is_on_ceiling():
+		jump()
 	
 	move_and_slide()
