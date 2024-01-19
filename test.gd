c4dc67324db914f1b6bca83e3382dafc749212d5
Tommy Rooney <tommyroons@gmail.com>
2024-01-18 19:39:39 -0600

    added animations and camera tracking

--- test.gd
+++ test.gd
@@ -1,9 +1,16 @@
 extends CharacterBody2D
 
-var speed = 100
+#setting character speed
+@export var speed = 100
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+#gives reference to the animated sprite node
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
+#setting a animation lock so one doesn't start without the other finishing first
+var animation_locked : bool = false
 
 # Our function for jumping so we don't have to repeat.
 func jump():
@@ -30,3 +37,11 @@ func _physics_process(delta):
 		jump()
 	
 	move_and_slide()
+	update_animation()
+
+func update_animation():
+	if not animation_locked:
+		if velocity.x != 0:
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("idle")
